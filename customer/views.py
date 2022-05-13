034d270a0dc573c939d289a566cf24725aaf9fb8
ReDiot69 <sthasubha23@gmail.com>
2022-05-13 17:11:10 +0545

    merged

--- customer/views.py
+++ customer/views.py
@@ -1,34 +1,51 @@
-from pickletools import read_uint1
 from django.shortcuts import render
 from customer.forms import CustomerForm
-from customer.models import Customer, Measurement
+from customer.models import Customer, Measurement, Order
+from employee.models import Employee
 
-# Create your views here.
+
 def measurement(request):
-    return render(request, 'measurement.html')
+    emp = Employee.objects.all()
+    context = {'employee': emp}
+    return render(request, 'measurement.html', context)
+
 
 def accounts(request):
     return render(request, 'accounts.html')
 
-def employee(request):
-    return render(request, 'employee.html')
 
 def customer(request):
-    return render(request,"customer.html")
+    cus = Customer.objects.all()
+    context = {'customer': cus}
+    return render(request, "customer.html", context)
 
-def dashboard(request):
-    return render(request,"dashboard.html")
-    
+
 def customer_store(request):
     form = CustomerForm(request.POST)
-    
     if form.is_valid():
-        Customer.objects.create(name=form.cleaned_data['name'],email=form.cleaned_data['email'],address=form.cleaned_data['address'],
-        number=form.cleaned_data['number'])
-    return render(request,'measurement.html')
+        c = Customer.objects.create(name=form.cleaned_data['name'], email=form.cleaned_data['email'],
+                                    address=form.cleaned_data['address'],
+                                    number=form.cleaned_data['number'])
+        m = Measurement.objects.create(customer=c, shoulder=form.cleaned_data['shoulder'],
+                                       full_length=form.cleaned_data['full_length'],
+                                       chest=form.cleaned_data['chest'], hip=form.cleaned_data['hip'],
+                                       sl=form.cleaned_data['sl'], m=form.cleaned_data['m'], ah=form.cleaned_data['ah'],
+                                       open=form.cleaned_data['open'], thigh=form.cleaned_data['thigh'],
+                                       knee=form.cleaned_data['knee'])
+        emp = Employee.objects.get(id=form.cleaned_data['employee'])
+        Order.objects.create(customer_measurement=m, deadline=form.cleaned_data['deadline'],
+                             employee=emp)
+        context = {'reg': True}
+    else:
+        context = {}
 
+    return render(request, 'measurement.html', context)
 
 
 def accounts_detail(request):
     pass
+
+
+def dashboard(request):
+    return render(request,"dashboard.html")
 
