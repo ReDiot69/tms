d8968e97e5fa0f64c6eac3113f19a580d733cc80
ReDiot69 <sthasubha23@gmail.com>
2022-07-24 18:07:44 +0545

    employee milauna try

--- vendor/models.py
+++ vendor/models.py
@@ -62,7 +62,7 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
     number = models.IntegerField(null=False)
     image = models.ImageField(max_length=255, null=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    # role = models.ForeignKey(Role, on_delete=models.CASCADE)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
--- vendor/views.py
+++ vendor/views.py
@@ -26,7 +26,7 @@ def reg_user(request):
                                        address=form.cleaned_data['c_address'], phone=form.cleaned_data['c_number'],
                                        logo=form.cleaned_data['c_image'])
         password = make_password(form.cleaned_data['password'])
-        role = Role.objects.get(id=1)
+        role = Role.objects.get(id=2)
         MyUser.objects.create(email=form.cleaned_data['email'], name=form.cleaned_data['name'],
                               password=password, address=form.cleaned_data['address'], vendor=vendor,
                               role=role, number=form.cleaned_data['number'])
@@ -35,20 +35,23 @@ def reg_user(request):
 
 
 def login_user(request):
-    form = UserLoginForm(request.POST)
-    if form.is_valid():
-        email = form.cleaned_data['email']
-        password = form.cleaned_data['password']
-        user = authenticate(email=email, password=password)
-        if user:
-            login(request, user)
-            return render(request, 'dashboard.html')
+    if request.method== 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(email=email, password=password)
+            if user:
+                login(request, user)
+                return render(request, 'dashboard.html')
+            else:
+                messages.info(request, 'No such account!')
+                return render(request, 'home.html', {'msg': True})
         else:
-            messages.info(request, 'No such account!')
-            return render(request, 'home.html', {'msg': True})
+            return render(request, 'home.html')
     else:
-        return render(request, 'home.html')
-
+            return render(request, 'home.html')
+    
 
 def logout_user(request):
     logout(request)
