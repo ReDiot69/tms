4e45cb3a081d378f13102b31fcc6b48b26d446d1
Yub <yubmaharjan@gmail.com>
2022-07-25 14:04:35 +0545

    employee detail display

--- employee/views.py
+++ employee/views.py
@@ -16,12 +16,18 @@ def reg_employ(request):
         password = make_password(form.cleaned_data['password'])
         vendor = request.user.vendor
         v = Vendor.objects.get(vendor=vendor)
-        role = Role.objects.get(id=2)
+        role = Role.objects.get(id=3)
         MyUser.objects.create(name=form.cleaned_data['name'], number=form.cleaned_data['number'],
                               address=form.cleaned_data['address'], email=form.cleaned_data['email'],
                               password=password, image=form.cleaned_data['image'],
                               role=role, vendor=v)
     return render(request, 'employee.html', {'reg_emp': True})
 
+
 def employeelanding(request):
-    return render(request, 'employeelanding.html')
+    user = request.user
+    print(user)
+    emp_detail = MyUser.objects.filter(vendor=user.vendor)
+    print( emp_detail)
+    context = {'emp_detail': emp_detail}
+    return render(request, 'employeelanding.html', context)
