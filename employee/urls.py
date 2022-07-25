fce578bd93bfe84df1f07776b73689cd8ecb61ac
ReDiot69 <sthasubha23@gmail.com>
2022-07-25 13:34:08 +0545

    employee landing to employee

--- employee/urls.py
+++ employee/urls.py
@@ -2,8 +2,8 @@ from  django.urls import path
 from .import views
 
 urlpatterns = [
-    path('employee', views.employee, name="call_employee_page"),
+    # path('employee', views.employee, name="call_employee_page"),
     path('reg_emp', views.reg_employ),
-    path('employeelanding', views.employeelanding, name="call_employee_page")
+    path('employee', views.employeelanding, name="call_employee_page")
  ]
 
