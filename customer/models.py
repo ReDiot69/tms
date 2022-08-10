67a6992edc742a904f4ae6764e93d989d4d15f66
Yub <yubmaharjan@gmail.com>
2022-08-10 17:19:56 +0545

    new models

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from django.db import models
 import datetime
 
@@ -33,16 +34,46 @@ class Measurement(models.Model):
     knee = models.FloatField(null=True)
     image = models.ImageField()
 
+STATUS_COM = (
+        (1, 'Not Complete'),
+        (2, 'Completed')
+    )
+
 
 class Order(models.Model):
     employee = models.ForeignKey('vendor.MyUser', on_delete=models.CASCADE)
     customer_measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE)
     orderdate = models.DateField(auto_now=False, default=datetime.date.today)
     deadline = models.DateField(auto_now=False)
+    status = models.IntegerField(choices=STATUS_COM, default=1)
 
 
-# class Invoice(models.Models):
-#     order =
-#     total =
-#     advance =
-#     remai = asdasd
+def create_id():
+    now = datetime.datetime.now()
+    return str(now.month)+str(now.day)+str(now.hour)+str(now.minute)+str(now.second)+str(int(uuid4()))[:1]
+
+
+class OrderedCategory(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    subtotal = models.DecimalField(max_digits=9, decimal_places=4)
+    status = models.IntegerField(choices=STATUS_COM, default=1)
+    description = models.TextField()
+
+
+class Invoice(models.Model):
+    STATUS = (
+        (1, 'Paid'),
+        (2, 'Not Paid')
+    )
+    id = models.CharField(max_length=20, primary_key=True, default=create_id, editable=False)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.date.today)
+    vat = models.IntegerField(default=13)
+    discount = models.FloatField()
+    total = models.DecimalField(decimal_places=4, max_digits=9)
+    advance = models.DecimalField(decimal_places=4, max_digits=9)
+    remaining = models.DecimalField(decimal_places=4, max_digits=9)
+    status = models.IntegerField(choices=STATUS, default=2)
+    ordercat = models.ManyToManyField(OrderedCategory)
