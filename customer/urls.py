5f12f5c3e2e39880dff5097e7d35088e48783b7e
ReDiot69 <sthasubha23@gmail.com>
2022-08-18 18:01:22 +0545

    acceptorder navside

--- customer/urls.py
+++ customer/urls.py
@@ -7,8 +7,9 @@ from . import views
 urlpatterns = [
     path('customer_store', views.customer_store, name="stores_data_to_db"),
     path('measurement', views.measurement, name="call_measurement_page"),
-    path('accounts_detail', views.accounts_detail, name="display_accountDetail_from_db"),
+    # path('accounts_detail', views.accounts_detail, name="display_accountDetail_from_db"),
     path('account', views.account, name="call_accounts_page"),
+    path('historyacc', views.historyacc, name="call_history_accounts_page"),
     path('acceptorder', views.acceptorder, name="call_accept_order_page"),
     path('order', views.order, name="call_order_page"),
     path('billing', views.billing, name="call_billing_page"),
